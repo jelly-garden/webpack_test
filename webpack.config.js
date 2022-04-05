acdb62c85a24c965c40bf3aca32b4ca52246584b
장정원 <longthegarden@innodep.com>
2022-04-05 15:52:07 +0900

    플러그인 설치 : html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js', // entry point
     output: {
         path: path.resolve(__dirname, 'dist'), // output directory
         filename: 'main.js' // output file name
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
 }
